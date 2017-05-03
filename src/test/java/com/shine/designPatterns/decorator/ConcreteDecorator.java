4d5c0811a0274d99381e4c22eac4e62bb66d1bf1
阿七 <747037402@qq.com>
2017-05-03 22:52:57 +0800

    增加对装饰模式的理解

--- src/test/java/com/shine/designPatterns/decorator/ConcreteDecorator.java
+++ src/test/java/com/shine/designPatterns/decorator/ConcreteDecorator.java
@@ -33,13 +33,17 @@ public class ConcreteDecorator extends Decorator{
     /**
      * 适用场景：在不影响其他对象的情况下，以动态、透明的方式给单个对象添加新功能、新职责场景下适合适用装饰模式，
      *           除此之外，当遇到不能采用继承方式对系统进行扩展或者采用继承不利于系统扩展的情况下也适合使用装饰者模式。
-     * 总结：  装饰模式降低了系统的耦合度，可以动态增加或删除对象的职责，
-     *        并使得需要装饰的具体构件类和具体装饰类可以独立变化，以便增加新的具体构件类和具体装饰类。
-     *        对于扩展一个对象的功能，装饰模式比继承更加灵活性，不会导致类的个数急剧增加。
+     *
+     * 运用 :  装饰模式将装饰的功能放在单独的类中，在需要的时间，让这个类去包装所需要的对象，可以有序的，有选择性的包装。
+     *
+     * 总结：  装饰模式降低了系统的耦合度，可以动态增加或删除对象的职责，有效的把类的核心职责和装饰功能区分开，而且可以
+     *         去除相关类中重复的装饰逻辑
+     *
      * @param args
      */
     public static void main(String[] args) throws FileNotFoundException {
-        ConcreteDecorator decorator=new ConcreteDecorator(new Iphone());
+        ConcreteDecorator2 decorator2=new ConcreteDecorator2(new Iphone());
+        ConcreteDecorator decorator=new ConcreteDecorator(decorator2);
         decorator.call();
 
         //InputStream就是装饰者模式中的超类（Component） ==>Phone
