a8a451dc4c375f691a7bd15f68458d94f82a6db2
阿七 <747037402@qq.com>
2017-05-12 00:18:46 +0800

    建造者模式demo和理解

---
+++ src/test/java/com/shine/designPatterns/builder/MinGuk.java
@@ -0,0 +1,28 @@
+package com.shine.designPatterns.builder;
+
+/**
+ * 表示被构造的复杂对象
+ * Created by 7le on 2017/5/11 0011.
+ */
+public class MinGuk {
+
+    private String smile;
+    private String moe;
+
+    public String getSmile() {
+        return smile;
+    }
+
+    public void setSmile(String smile) {
+        this.smile = smile;
+    }
+
+    public String getMoe() {
+        return moe;
+    }
+
+    public void setMoe(String moe) {
+        this.moe = moe;
+    }
+
+}
