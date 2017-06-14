cae1d3a130850dcbbcb151e58d90fe43e75fd628
7le <747037402@qq.com>
2017-06-14 20:37:06 +0800

    适配器模式demo -- 附带有趣的小故事

---
+++ src/test/java/com/shine/designPatterns/adapter/ObjectAdapter.java
@@ -0,0 +1,20 @@
+package com.shine.designPatterns.adapter;
+
+/**
+ * 对象适配器
+ * Created by 7le on 017/6/14
+ */
+public class ObjectAdapter implements Target{
+
+    private Adaptee adaptee;
+
+    // 可以通过构造函数传入具体需要适配的被适配类对象
+    public ObjectAdapter (Adaptee adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public void method() {
+        this.adaptee.specialMethod();
+    }
+}
