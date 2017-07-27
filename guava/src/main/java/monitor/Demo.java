09b09d455fcd8222add801d9234cdb73186a8cb4
7le <747037402@qq.com>
2017-07-27 16:36:49 +0800

    guava monitor demo

---
+++ guava/src/main/java/monitor/Demo.java
@@ -0,0 +1,107 @@
+package monitor;
+
+import com.google.common.util.concurrent.Monitor;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Monitor 类似 synchronized ReentrantLock
+ */
+public class Demo {
+
+    //使用synchronized
+    public class SafeBox1<V> {
+        private V value;
+
+        public synchronized V get() throws InterruptedException {
+            while (value == null) {
+                wait();
+            }
+            V result = value;
+            value = null;
+            notifyAll();
+            return result;
+        }
+
+        public synchronized void set(V newValue) throws InterruptedException {
+            while (value != null) {
+                wait();
+            }
+            value = newValue;
+            notifyAll();
+        }
+    }
+
+    //使用 ReentrantLock
+    public class SafeBox2<V> {
+        private final ReentrantLock lock = new ReentrantLock();
+        private final Condition valuePresent = lock.newCondition();
+        private final Condition valueAbsent = lock.newCondition();
+        private V value;
+
+        public V get() throws InterruptedException {
+            lock.lock();
+            try {
+                while (value == null) {
+                    valuePresent.await();
+                }
+                V result = value;
+                value = null;
+                valueAbsent.signal();
+                return result;
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        public void set(V newValue) throws InterruptedException {
+            lock.lock();
+            try {
+                while (value != null) {
+                    valueAbsent.await();
+                }
+                value = newValue;
+                valuePresent.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    //使用Monitor
+    public class SafeBox3<V> {
+        private final Monitor monitor = new Monitor();
+        private final Monitor.Guard valuePresent = new Monitor.Guard(monitor) {
+            public boolean isSatisfied() {
+                return value != null;
+            }
+        };
+        private final Monitor.Guard valueAbsent = new Monitor.Guard(monitor) {
+            public boolean isSatisfied() {
+                return value == null;
+            }
+        };
+        private V value;
+
+        public V get() throws InterruptedException {
+            monitor.enterWhen(valuePresent);
+            try {
+                V result = value;
+                value = null;
+                return result;
+            } finally {
+                monitor.leave();
+            }
+        }
+
+        public void set(V newValue) throws InterruptedException {
+            monitor.enterWhen(valueAbsent);
+            try {
+                value = newValue;
+            } finally {
+                monitor.leave();
+            }
+        }
+    }
+}
