88f0d7f10be2c1174244057441720428ec58ad3c
7le <747037402@qq.com>
2017-08-29 20:24:24 +0800

    添加epc状态码

---
+++ epc/src/main/java/shine/epc/EpcCode.java
@@ -0,0 +1,53 @@
+package shine.epc;
+
+/**
+ * EPC 状态码
+ */
+public class EpcCode {
+	/**
+	 * 操作成功，成功的通用返回码
+	 */
+	public static final int SUCCESS = 0;
+
+	/**
+	 * 未知错误
+	 */
+	public static final int UNKNOWN_ERR = 9999;
+
+	/**
+	 * 非法的SessionId
+	 */
+	public static final int INVALID_SESSION_ID = 1001;
+
+	/**
+	 * 非法的消息格式
+	 */
+	public static final int INVALID_MSG_FORMAT = 1002;
+
+	/**
+	 * 系统内部错误
+	 */
+	public static final int INTERNAL_ERR = 2001;
+
+	/**
+	 * 数据库错误
+	 */
+	public static final int DB_ERR = 2101;
+
+	/**
+	 * 通讯错误
+	 */
+	public static final int SOCKET_ERR = 2201;
+
+	/**
+	 * 非法事件参数
+	 */
+	public static final int INVALID_PARAM = 2301;
+	
+	/**
+	 * 服务器状态异常
+	 */
+	public static final int SERVER_STATUS_ERROR = 2302;
+
+	
+}
